920734c7131ef5636e4c23892a8dcee92548e1e2
Matt Melling <mattmelling@fastmail.com>
2021-02-09 18:59:55 +0000

    added homeManagerModules

--- flake.nix
+++ flake.nix
@@ -10,5 +10,38 @@
       overlay = final: prev: {
         mpvyt = prev.callPackage ./default.nix { };
       };
+      homeManagerModules = {
+        mpvyt = { pkgs, config, lib, ... }: let
+          cfg = config.services.mpvyt;
+        in
+          {
+            options = with lib.types; {
+              services.mpvyt = {
+                enable = lib.mkEnableOption "mpvyt";
+                playlist = lib.mkOption {
+                  type = str;
+                  description = "Where to store playlist file";
+                };
+              };
+            };
+            config = lib.mkIf cfg.enable {
+              systemd.user.services.mpvyt = {
+                Unit = {
+                  Description = "mpvyt";
+                };
+                Install = {
+                  WantedBy = [ "graphical-session.target" ];
+                };
+                Service = {
+                  Type = "simple";
+                  ExecStart = "${pkgs.mpvyt}/bin/mpvyt";
+                  ExecStop = "${pkgs.killall}/bin/killall mpvyt";
+                  Restart = "always";
+                  Environment = "MPV_PLAYLIST_PATH=${cfg.playlist}";
+                };
+              };
+            };
+          };
+      };
     };
 }
